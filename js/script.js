0e996d576916fd8a412d1851f15b9092ab49eb68
soyun5340 <soyun5340@gmail.com>
2024-10-25 17:01:19 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,3 +6,43 @@ $(".popup_open a").click(function(){
 $(".popup_close").click(function(){
     $("#popup").fadeOut(0)
 });
+// popup2
+$("#popup2").hide();
+$(".popup_open2 a").click(function(){
+    $("#popup2").fadeIn(0)
+});
+$(".popup_close").click(function(){
+    $("#popup2").fadeOut(0)
+});
+// popup3
+$("#popup3").hide();
+$(".popup_open3 a").click(function(){
+    $("#popup3").fadeIn(0)
+});
+$(".popup_close").click(function(){
+    $("#popup3").fadeOut(0)
+});
+// popup4
+$("#popup4").hide();
+$(".popup_open4 a").click(function(){
+    $("#popup4").fadeIn(0)
+});
+$(".popup_close").click(function(){
+    $("#popup4").fadeOut(0)
+});
+// popup5
+$("#popup5").hide();
+$(".popup_open5 a").click(function(){
+    $("#popup5").fadeIn(0)
+});
+$(".popup_close").click(function(){
+    $("#popup5").fadeOut(0)
+});
+// popup6
+$("#popup6").hide();
+$(".popup_open6 a").click(function(){
+    $("#popup6").fadeIn(0)
+});
+$(".popup_close").click(function(){
+    $("#popup6").fadeOut(0)
+});
